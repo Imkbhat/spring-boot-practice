9f2b2ce75b23a1fd26e675761d0dcbca6c40519f
Imkbhat <kbs71190@gmail.com>
2021-08-06 16:06:55 +0530

    initial commit

---
+++ src/test/java/com/luv2codeinjava/boot/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2codeinjava.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
